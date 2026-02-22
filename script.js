32842300a7eb1447f46de817e027ca196d23023a
KAprachir <kaprachir23@gmail.com>
2026-02-22 16:09:34 +0600

    no job section completed added in filter btn

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
 const interviewList = [];
 const rejectedList = [];
+
+// containers
+const cardSection = document.getElementById("card-container");
+const mainContainer = document.querySelector("main");
+const noJob = document.getElementById("no-job");
+
+// countdown
 const totalCount = document.getElementById("total-count");
 const interviewCount = document.getElementById("interview-count");
 const rejectedCount = document.getElementById("rejected-count");
 
-const cardSection = document.getElementById("card-container");
-
-const mainContainer = document.querySelector("main");
-console.log(mainContainer);
+// filter buttons
+const allBtn = document.getElementById("all-btn");
+const interviewBtn = document.getElementById("interview-btn");
+const rejectedBtn = document.getElementById("rejected-btn");
 
 function calculateCount() {
   totalCount.innerText = cardSection.children.length;
-//   interviewCount.innerText = interviewList.children.length;
-//   rejectedCount.innerText = rejectedList.children.length;
+  // interviewCount.innerText = interviewList.children.length;
+  // rejectedCount.innerText = rejectedList.children.length;
 }
 calculateCount();
 
+// toogling style
+function toogleStyle(id) {
+  allBtn.classList.add("btn-outline");
+  interviewBtn.classList.add("btn-outline");
+  rejectedBtn.classList.add("btn-outline");
+
+  const seletedBtn = document.getElementById(id);
+  seletedBtn.classList.remove("btn-outline");
+}
+
+// no job adding in filter button
+allBtn.addEventListener("click", function () {
+  noJob.classList.add("hidden");
+  cardSection.classList.remove("hidden");
+});
+interviewBtn.addEventListener("click", function (id) {
+  cardSection.classList.add("hidden");
+  noJob.classList.remove("hidden");
+});
+rejectedBtn.addEventListener("click", function (id) {
+  cardSection.classList.add("hidden");
+  noJob.classList.remove("hidden");
+});
